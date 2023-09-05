de36b75000c65876ac9db07ee92290e8caa099fc
Martin N <me77@uclive.ac.nz>
2023-09-06 01:13:21 +1200

    added board eval files

---
+++ ChessEngine/BoardEvaluation.cpp
@@ -0,0 +1,6 @@
+#include "BoardEvaluation.h"
+
+float BoardEvaluation::getEvaluation(const ChessBoard* board, bool forWhite)
+{
+    return 0.0f;
+}
---
+++ ChessEngine/BoardEvaluation.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "ChessBoard.h"
+
+class BoardEvaluation
+{
+
+	float getEvaluation(const ChessBoard* board, bool forWhite);
+
+};
+
+
+
